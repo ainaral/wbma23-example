06bbc23ed0f4ab1340be07e3d18c0d7ea05f244d
Ainara Larrañaga <ainara.larranagaflores@metropolia.fi>
2023-01-12 09:46:38 +0200

    .

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,4 +1,4 @@
-import {Image, StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {Image, View, Text, TouchableOpacity} from 'react-native';
 import PropTypes from 'prop-types';
 
 const ListItem = ({singleMedia}) => {
