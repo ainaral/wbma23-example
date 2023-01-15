e1ace73f38e5a7d4b893907deb67a40467e55f50
Ainara Larrañaga <ainara.larranagaflores@metropolia.fi>
2023-01-15 19:41:06 +0200

    Tab navigator done

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
 import {StatusBar} from 'expo-status-bar';
-import {Platform, SafeAreaView, StyleSheet} from 'react-native';
-import List from './components/List';
+import Navigator from './navigators/Navigator';
 
 const App = () => {
-  //  console.log('App starting!')
   return (
-    <SafeAreaView style={styles.container}>
-      <List />
+    <>
+      <Navigator />
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-  },
-});
