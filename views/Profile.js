e73640fe16606061d8d5ea11e1943abc260f1a95
Ainara Larrañaga <ainara.larranagaflores@metropolia.fi>
2023-01-19 11:48:45 +0200

    added basic profile screen info and user state

--- views/Profile.js
+++ views/Profile.js
@@ -4,14 +4,18 @@ import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
 
 const Profile = () => {
-  const {setIsLoggedIn} = useContext(MainContext);
+  const {setIsLoggedIn, user, setUser} = useContext(MainContext);
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
+      <Text>Username: {user.username}</Text>
+      <Text>Email: {user.email}</Text>
+      <Text>Full name: {user.full_name}</Text>
       <Button
         title="Logout!"
         onPress={async () => {
           console.log('Loggin out!');
+          setUser({});
           setIsLoggedIn(false);
           try {
             await AsyncStorage.clear();
