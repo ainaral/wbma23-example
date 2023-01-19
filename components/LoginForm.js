0c71764d58849c0fe717aff11610232d77b48525
Ainara Larrañaga <ainara.larranagaflores@metropolia.fi>
2023-01-19 10:20:23 +0200

    login form done

---
+++ components/LoginForm.js
@@ -0,0 +1,81 @@
+import React, {useContext} from 'react';
+import PropTypes from 'prop-types';
+import {Button, Text, TextInput, View} from 'react-native';
+import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useAuthentication} from '../hooks/ApiHooks';
+import {Controller, useForm} from 'react-hook-form';
+
+const LoginForm = (props) => {
+  const {setIsLoggedIn} = useContext(MainContext);
+  const {postLogin} = useAuthentication();
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {username: '', password: ''},
+  });
+
+  const logIn = async (loginData) => {
+    console.log('Login button pressed', loginData);
+    console.log('validation errors', errors);
+    const data = {
+      // username: 'ainaral',
+      // password: 'Lu-na-55',
+    };
+    try {
+      const loginResult = await postLogin(loginData);
+      console.log('logIn', loginResult);
+      await AsyncStorage.setItem('userToken', loginResult.token);
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.error('logIn', error);
+      // TODO: notify user about failed login attempt
+    }
+  };
+
+  return (
+    <View>
+      <Text>Login form</Text>
+      <Controller
+        control={control}
+        rules={{required: true, minLength: 3}}
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            placeholder="Username"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="username"
+      />
+      {errors.username?.type === 'required' && <Text>is required</Text>}
+      {errors.username?.type === 'minLength' && (
+        <Text>min Length is 3 characters</Text>
+      )}
+
+      <Controller
+        control={control}
+        rules={{required: true, minLength: 5}}
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            placeholder="Password"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry={true}
+          />
+        )}
+        name="password"
+      />
+      {errors.password && <Text>Password (min.5 chars) is required.</Text>}
+      <Button title="Sign in!" onPress={handleSubmit(logIn)} />
+    </View>
+  );
+};
+
+LoginForm.propTypes = {};
+
+export default LoginForm;
